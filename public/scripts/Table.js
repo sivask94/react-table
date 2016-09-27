a58b1441e75bcde63d2f3e0776dde1d66b6e1374
sivaprakash <sivask94@gmail.com>
2016-09-27 17:50:03 +0530

    node modules installed and table has been created

--- public/scripts/Table.js
+++ public/scripts/Table.js
@@ -1,72 +1,75 @@
 var headers = ["Name", "Quantity", "Price"];
-var rows = [{Name : Rice, Quantity : '2kg', Price : 100},
-  {"wheat","4kg","150"},{"Tomato","6kg",80}];
+var rows = [
+    ["Rice", "2kg", 100],
+    ["wheat", "4kg", 150],
+    ["Tomato", "6kg", 8]
+];
 
 var HeaderCell = React.createClass({
-  render : function() {
-    return (
-      <td>{this.props.data}</td>
-    );
-  }
+    render: function () {
+        return (
+            <th>{this.props.data}</th>
+        );
+    }
 });
 
 var TableHeader = React.createClass({
-  render : function() {
-    return (
-      <th>
-        {this.props.head.map(function (data, idx) {
-          return <HeaderCell data={data}/>;
-        })}
-      </th>
-    );
-  }
+    render: function () {
+        return (
+            <tr>
+                {this.props.head.map(function (data, idx) {
+                    return <HeaderCell data={data}/>;
+                })}
+            </tr>
+        );
+    }
 });
 
 var Rowcell = React.createClass({
-  render: function () {
-    return (
-
-        <td>{this.props.cell}</td>
-    );
-  }
-});
-
-var Row = React.createClass({
-  render: function () {
-    return (
-      <tr>
-        {this.props.dat.map(function (data, idx) {
-          return <Rowcell cell={data}/>;
-        })}
-      </tr>
-    );
-  }
+    render: function () {
+        return (
+            <td>{this.props.cell}</td>
+        );
+    }
 });
 
 var TableRows = React.createClass({
-  render: function () {
-    return (
-      <tr>
-        {this.props.row.map(function (data, idx) {
-          return <Row dat={data}/>;
-        })}
-      </tr>
-    );
-  }
+    getRow: function (row) {
+        return (
+            <tr>
+                {
+                    row.map(function (elem) {
+                        return <Rowcell cell={elem}/>;
+                    })
+                }
+            </tr>
+        );
+    },
+    render: function () {
+        var self = this;
+
+        return (
+            <tbody>
+            {this.props.rows.map(function (row) {
+                return self.getRow(row)
+            })}
+            </tbody>
+        );
+    }
 });
 
 var Table = React.createClass({
-  render : function() {
-    return (
-      <table>
-        <thead><TableHeader head={headers}/></thead>
-        <tbody><TableRows row={rows}/></tbody>
-      </table>
-    );
-  }
+    render: function () {
+        return (
+            <table>
+                <thead><TableHeader head={headers}/></thead>
+                <TableRows rows={rows}/>
+            </table>
+        );
+    }
 });
 
 ReactDOM.render(
-  <Table/>,
-  document.getElementById('content')
+    <Table/>,
+    document.getElementById('content')
 );
